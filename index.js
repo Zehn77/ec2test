cc89a9309aad50ab97909df07cf7682f83c1c2d5
Zehn77 <zekhn777@gmail.com>
2026-02-18 13:31:01 +0500

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
   res.end(
-    "<h1>Assalomu alaykum!!!!!!!! Node.js AWS serverda muvaffaqiyatli ishga tushdi!</h1>",
+    "<h1>Assalomu alaykum! Node.js AWS serverda muvaffaqiyatli ishga tushdi!</h1>",
   );
 });
 
