8673a4f0f39ea61708b22ddadc5e03bc01b80169
Zehn77 <zekhn777@gmail.com>
2026-02-18 13:28:03 +0500

    test

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  res.end("<h1>Salom! Node.js AWS serverda muvaffaqiyatli ishga tushdi!</h1>");
+  res.end(
+    "<h1>Assalomu alaykum! Node.js AWS serverda muvaffaqiyatli ishga tushdi!</h1>",
+  );
 });
 
 // Server 3000-portda va barcha tarmoq interfeyslarida tinglaydi
 server.listen(3000, "0.0.0.0", () => {
-  console.log("Server ishlayapti: http://your-public-ip:3000");
+  console.log("Serverda juda yaxshi ishlayapti: http://your-public-ip:3000");
 });
